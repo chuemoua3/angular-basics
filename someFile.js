88e5e0076a2abb1cc23910e3c39c43e001e3bd6f
chuemoua3 <chuemoua12@outlook.com>
2020-05-26 21:36:31 -0400

    finished typescript hw and angular intro lab

--- someFile.js
+++ someFile.js
@@ -1,11 +1,11 @@
-function sayHello(person) {
-    return "Hello, " + person;
-}
-var user = "Super Student";
-document.getElementById("para").innerHTML = sayHello(user);
+// function sayHello(person: string): string{
+//     return "Hello, " + person;
+// }
+// var user = "Super Student";
+// document.getElementById("id1").textContent = sayHello(user);
 // new Person class
 var Person = /** @class */ (function () {
-    function Person(firstName, lastName, age, phoneNum, state, zipCode, occupation) {
+    function Person(firstName, lastName, age, phoneNum, state, zipCode, occupation, hourly) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
@@ -13,6 +13,7 @@ var Person = /** @class */ (function () {
         this.state = state;
         this.zipCode = zipCode;
         this.occupation = occupation;
+        this.hourly = hourly;
     }
     //setters and getters
     Person.prototype.getFirstName = function () {
@@ -57,6 +58,9 @@ var Person = /** @class */ (function () {
     Person.prototype.setOccupation = function (occupation) {
         this.occupation = occupation;
     };
+    Person.prototype.getCerts = function () {
+        return this.certs;
+    };
     //other methods
     Person.prototype.getFullName = function () {
         return this.firstName + " " + this.lastName;
@@ -64,5 +68,50 @@ var Person = /** @class */ (function () {
     Person.prototype.getLocation = function () {
         return this.state + " " + this.zipCode;
     };
+    //function to calculate weekly wage and takes the number of hours as parameters
+    Person.prototype.getWeeklyRate = function (hours) {
+        if (hours) {
+            return this.hourly * hours;
+        }
+        else {
+            return this.hourly * 40;
+        }
+    };
+    //function to get name and wage
+    Person.prototype.getHourlyWage = function (wage) {
+        if (wage) {
+            return this.getFullName() + " " + this.getWeeklyRate(wage).toString();
+        }
+        else {
+            return this.getFullName() + " weekly wage: $" + this.getHourlyWage().toString();
+        }
+    };
+    //creating a property of a person's certifications/degrees that is an array
+    //can be things such as MBA, CSM, MCSD, etc.
+    Person.prototype.addCerts = function (certs) {
+        for (var cert in certs) {
+            certs.push(cert);
+        }
+        this.certs = certs;
+    };
+    //interface method
+    Person.createPerson = function (person) {
+        var newPerson = new Person(person.firstName, person.lastName);
+        if (person.firstName) {
+            newPerson.firstName;
+        }
+        if (person.lastName) {
+            newPerson.lastName;
+        }
+        return newPerson;
+    };
     return Person;
-}());
+}()); //end of Person class
+//outputs
+var tom = new Person("Tom", "Brady", "42", "7041234555", "NC", "28262", "NFL QB", 30);
+var peyton = new Person("Peyton", "Manning", "44", "7045552222", "NC", "28202", "Media Analyst", 15);
+var wade = new Person("Dwyane", "Wade", "37", "7042928222", "CA", "90210", "NBA HOF", 25);
+document.getElementById("id2").textContent = tom.getFullName();
+document.getElementById("id3").textContent = peyton.getOccupation();
+document.getElementById("id4").textContent = wade.getAge();
+document.getElementById("id5").textContent = Person.createPerson({ firstName: "Jack", lastName: "Sparrow" }).getFullName();
--- someFile.ts
+++ someFile.ts
@@ -1,22 +1,35 @@
-function sayHello(person: string): string {
-    return "Hello, " + person;
-   }
-   var user = "Super Student";
-   document.getElementById("para").innerHTML = sayHello(user);
 
+// function sayHello(person: string): string{
+//     return "Hello, " + person;
+// }
+// var user = "Super Student";
+// document.getElementById("id1").textContent = sayHello(user);
+
+    //interface function
+    interface PersonOptions{
+        firstName: string;
+        lastName: string;
+        age?: string;
+        phoneNum?: string;
+        state?: string;
+        zipCode?: string;
+        occupation?: string;
+        hourly?: number;
+    }
 
    // new Person class
    class Person{
        private firstName: string;
        private lastName: string;
-       private  age: number;
-       private  phoneNum: number;
+       private  age: string;
+       private  phoneNum: string;
        private state: string;
-       private zipCode: number;
+       private zipCode: string;
        private occupation: string;
        private hourly: number;
+       private certs: string[];
 
-       constructor(firstName: string, lastName: string, age: number, phoneNum: number, state: string, zipCode: number, occupation: string){
+       constructor(firstName: string, lastName: string, age?: string, phoneNum?: string, state?: string, zipCode?: string, occupation?: string, hourly?: number){
            this.firstName = firstName;
            this.lastName = lastName;
            this.age = age;
@@ -24,6 +37,7 @@ function sayHello(person: string): string {
            this.state = state;
            this.zipCode = zipCode;
            this.occupation = occupation;
+           this.hourly = hourly;
        }
 
        //setters and getters
@@ -42,17 +56,17 @@ function sayHello(person: string): string {
            this.lastName = lastName;
        }
 
-       getAge(): number{
+       getAge(): string{
            return this.age;
        }
-       setAge(age: number): void{
+       setAge(age: string): void{
            this.age = age;
        }
 
-         getPhoneNum(): number{
+         getPhoneNum(): string{
            return this.phoneNum;
        }
-       setPhoneNum(phoneNum: number): void{
+       setPhoneNum(phoneNum: string): void{
            this.phoneNum = phoneNum;
        }
 
@@ -63,10 +77,10 @@ function sayHello(person: string): string {
            this.state = state;
        }
 
-       getZipCode(): number{
+       getZipCode(): string{
            return this.zipCode;
        }
-       setZipCode(zipCode: number): void{
+       setZipCode(zipCode: string): void{
            this.zipCode = zipCode;
        }
 
@@ -76,6 +90,11 @@ function sayHello(person: string): string {
        setOccupation(occupation: string): void{
            this.occupation = occupation;
        }
+
+       getCerts(): string[]{
+           return this.certs;
+       }
+
 
        //other methods
 
@@ -87,9 +106,58 @@ function sayHello(person: string): string {
            return this.state + " " + this.zipCode;
        }
 
+  
+
+       //function to calculate weekly wage and takes the number of hours as parameters
+        getWeeklyRate(hours?: number): number{
+           if(hours){
+               return this.hourly * hours;
+           }else {
+               return this.hourly * 40;
+           }
+       }
+       //function to get name and wage
+       getHourlyWage(wage?: number): string{
+           if(wage){
+               return this.getFullName() + " " + this.getWeeklyRate(wage).toString();
+           }else {
+               return this.getFullName() + " weekly wage: $" + this.getHourlyWage().toString(); 
+           }
+       }
+
+       //creating a property of a person's certifications/degrees that is an array
+       //can be things such as MBA, CSM, MCSD, etc.
+       addCerts(certs: string[]): void{
+           for(let cert in certs){
+               certs.push(cert);
+           }
+           this.certs = certs;
+       }
+
+            //interface method
+            static createPerson(person: PersonOptions): Person{
+                let newPerson = new Person(person.firstName, person.lastName);
+                if(person.firstName){
+                    newPerson.firstName;
+                }
+                if(person.lastName){
+                    newPerson.lastName;
+                }
+                return newPerson;
+            }
 
 
-       
+}//end of Person class
 
 
-}
+
+//outputs
+var tom = new Person("Tom", "Brady", "42", "7041234555", "NC", "28262", "NFL QB", 30);
+var peyton = new Person("Peyton", "Manning", "44", "7045552222", "NC", "28202", "Media Analyst", 15);
+var wade = new Person("Dwyane", "Wade", "37", "7042928222", "CA", "90210", "NBA HOF", 25);
+
+document.getElementById("id2").textContent = tom.getFullName();
+document.getElementById("id3").textContent = peyton.getOccupation();
+document.getElementById("id4").textContent = wade.getAge();
+document.getElementById("id5").textContent = Person.createPerson({firstName: "Jack", lastName: "Sparrow"}).getFullName();
+
